fd0108790ffe9b188b68cd2246e2ffe93f00e603
lxtatarkin <tatarkin.dev@gmail.com>
2025-11-22 09:55:56 +0100

    fix bot.py

--- bot.py
+++ bot.py
@@ -554,7 +554,7 @@ async def main():
             )
             return
 
-        await message.answer("Конвертирую документ...")
+        await message.answer("Конвертирую в PDF...")
 
         file = await bot.get_file(doc_msg.file_id)
         src_path = FILES_DIR / filename
@@ -821,6 +821,9 @@ async def main():
         if not await check_size_or_reject(message, photo.file_size):
             return
 
+        # статус
+        await message.answer("Конвертирую фото в PDF...")
+
         file = await bot.get_file(photo.file_id)
 
         # делаем имя файла, чтобы сохранить на диск
@@ -828,7 +831,6 @@ async def main():
         src_path = FILES_DIR / filename
         await bot.download_file(file.file_path, destination=src_path)
 
-        # сейчас для фоток делаем то же самое, что и для "Документ/фото → PDF":
         pdf_path = image_file_to_pdf(src_path)
         if not pdf_path:
             await message.answer("Не удалось конвертировать изображение.")
